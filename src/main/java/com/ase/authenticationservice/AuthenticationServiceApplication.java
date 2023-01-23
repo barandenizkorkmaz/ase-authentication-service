0a1b7d25d74db87510aef5c469ed7da7ae0b0485
ge28vob <deniz.korkmaz@tum.de>
2023-01-23 16:50:29 +0100

    Add cors

--- src/main/java/com/ase/authenticationservice/AuthenticationServiceApplication.java
+++ src/main/java/com/ase/authenticationservice/AuthenticationServiceApplication.java
@@ -3,7 +3,10 @@ package com.ase.authenticationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -12,6 +15,16 @@ public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
